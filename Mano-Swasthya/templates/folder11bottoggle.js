12874f0175d7134242166a97c062336519584812
JagdishPatil23 <heyitsjagdishp.23@gmail.com>
2025-06-19 13:48:44 +0530

    Delete Mano-Swasthya directory

--- Mano-Swasthya/templates/folder11bottoggle.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("chatbotToggle").addEventListener("click", function() {
-    document.getElementById("chatbot").style.display = "flex"
-});
-
-document.getElementById("closeChatbot").addEventListener("click", function() {
-    document.getElementById("chatbot").style.display = "none"
-});
